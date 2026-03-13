6598db6c78d3199c86971a529650e67d1efaa819
byteoxo <byteoxxo@gmail.com>
2026-03-13 07:03:16 +0000

    Update binance-ticker to v1.2.2

--- Formula/binance-ticker.rb
+++ Formula/binance-ticker.rb
@@ -2,25 +2,25 @@ class BinanceTicker < Formula
   desc "Terminal-based Binance futures/spot price ticker with real-time WebSocket updates"
   homepage "https://github.com/byteoxo/binance-ticker"
   license "MIT"
-  version "1.2.0"
+  version "1.2.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/byteoxo/binance-ticker/releases/download/v1.2.0/binance-ticker_darwin_arm64.tar.gz"
-      sha256 "94a173551d37b0b4cf18ebe525c861d63291e6d79518c30127c8b41248d03068"
+      url "https://github.com/byteoxo/binance-ticker/releases/download/v1.2.2/binance-ticker_darwin_arm64.tar.gz"
+      sha256 "eb654e9cbbf946557a6508e2e936e3cfcb9388f271334a411b9759af8b3f8919"
     else
-      url "https://github.com/byteoxo/binance-ticker/releases/download/v1.2.0/binance-ticker_darwin_amd64.tar.gz"
-      sha256 "5970b19806880bde3dc097e978cd3bef2babf5b45fa9aed111dc965eaea86523"
+      url "https://github.com/byteoxo/binance-ticker/releases/download/v1.2.2/binance-ticker_darwin_amd64.tar.gz"
+      sha256 "6d796d8822fa849b8610b529b5e5dbabc47554d50d124d524adecd974c061a83"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/byteoxo/binance-ticker/releases/download/v1.2.0/binance-ticker_linux_arm64.tar.gz"
-      sha256 "2000b765fa480a414f64a91041bd0dfccd5d75927872a0abc8bcadc30eb6ae8d"
+      url "https://github.com/byteoxo/binance-ticker/releases/download/v1.2.2/binance-ticker_linux_arm64.tar.gz"
+      sha256 "d69924cd91dcd32b331333b584cdcff66cd649dc3d8e133ef439ca5f62d5a3f5"
     else
-      url "https://github.com/byteoxo/binance-ticker/releases/download/v1.2.0/binance-ticker_linux_amd64.tar.gz"
-      sha256 "133a6241cd0cee578752d7f4cb84986c4acde33dff3e4e4d6eac84e5409616e2"
+      url "https://github.com/byteoxo/binance-ticker/releases/download/v1.2.2/binance-ticker_linux_amd64.tar.gz"
+      sha256 "fbaaf16b10ccecca54e2ba80b83452d83f37b9e5fc70de9b1d9988f2c66c5b3c"
     end
   end
 
