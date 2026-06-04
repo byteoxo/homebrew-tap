84b2abd5d6f50ce9827d1dc9258f447faecfc41b
byteoxo <byteoxxo@gmail.com>
2026-06-04 13:11:31 +0000

    Update crypto-ticker to v1.9.0

--- Formula/crypto-ticker.rb
+++ Formula/crypto-ticker.rb
@@ -2,25 +2,25 @@ class CryptoTicker < Formula
   desc "Terminal-based crypto futures/spot price ticker supporting Binance and Gate.io"
   homepage "https://github.com/byteoxo/crypto-ticker"
   license "MIT"
-  version "1.8.4"
+  version "1.9.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/byteoxo/crypto-ticker/releases/download/v1.8.4/crypto-ticker_darwin_arm64.tar.gz"
-      sha256 "613525c2f4ce9c224e0ed58ec97fe19485ec7d738e8622f0f4608a02404d8eeb"
+      url "https://github.com/byteoxo/crypto-ticker/releases/download/v1.9.0/crypto-ticker_darwin_arm64.tar.gz"
+      sha256 "077cb04be4efb3bd386f8783e09231d4efc9b7412d37ffeeeddd1b291eb67ad9"
     else
-      url "https://github.com/byteoxo/crypto-ticker/releases/download/v1.8.4/crypto-ticker_darwin_amd64.tar.gz"
-      sha256 "48fe3bbfde02cf80d74a4452dc88012014e136994fa275ae0748fbe424088ed9"
+      url "https://github.com/byteoxo/crypto-ticker/releases/download/v1.9.0/crypto-ticker_darwin_amd64.tar.gz"
+      sha256 "43b5340ea8662cabf94e96c4d90c210b2029afa4b2e800560d0a0b2c261dccd7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/byteoxo/crypto-ticker/releases/download/v1.8.4/crypto-ticker_linux_arm64.tar.gz"
-      sha256 "3b29d7cf6634732dec8cf698ad2bceb6fc5a1d80346a8c7f5bb9342040575ca7"
+      url "https://github.com/byteoxo/crypto-ticker/releases/download/v1.9.0/crypto-ticker_linux_arm64.tar.gz"
+      sha256 "9bad06f7550cc22377a7d8cbc6bad8f8782288e92d2e274bb6171d7a0cae7526"
     else
-      url "https://github.com/byteoxo/crypto-ticker/releases/download/v1.8.4/crypto-ticker_linux_amd64.tar.gz"
-      sha256 "23fdc152c63ad211c62a38ef1561b62b08c132c07b6a9f20f3673b3f6c1432a1"
+      url "https://github.com/byteoxo/crypto-ticker/releases/download/v1.9.0/crypto-ticker_linux_amd64.tar.gz"
+      sha256 "8c7d4682c09afc8bb789034dc8191a83f233a39b475040b90d96deb36a358cb7"
     end
   end
 
